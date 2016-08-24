7444a6a1b9cabda167dcd76d3348502ff5342743
cspk <cspk@tembosystems.com>
2016-08-24 14:30:08 +0530

    Remove user details 	modified:   README.md 	modified:   config/initializers/setup_mailer.rb

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Conference-booking
+
+Add your mailer email-id and password in 
+
+config/initializers/setup_mailer.rb
--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
    :domain => "gmail.com",
    :authentication => "plain",
    :enable_starttls_auto => true,
-   :user_name => "preethikapriya9@gmail.com",
-   :password => "lrdvuxegfdrusmvg",
+   :user_name => ""
+   :password => ""
    :openssl_verify_mode  => 'none'
 }
